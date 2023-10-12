80391abcc05a8051ba2b4c4f6f74c005774c2aaf
BaiFengLinShi <115349874+BaiFengLinShi@users.noreply.github.com>
2023-10-12 16:57:01 +0800

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -10,7 +10,7 @@ let repos = {
     add: (repo) => {
         if (repos.content.indexOf(repo) != -1) return;
         const stringsToRemove = ["git@github.com:", "test.git", "https://github.com/"];
-        repos.content = repos.content.filter(repo => !stringsToRemove.some(str => repo.includes(str)));
+        repos = repos.content.filter(repo => !stringsToRemove.some(str => repo.includes(str)));
         repos.content.push(repo);
         repos.save();
         repos.load();
