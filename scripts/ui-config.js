dd2f47f60d4f198866ed0a174016c2465deac225
BaiFengLinShi <115349874+BaiFengLinShi@users.noreply.github.com>
2023-10-12 15:55:13 +0800

    Update ui-config.js

--- scripts/ui-config.js
+++ scripts/ui-config.js
@@ -1,15 +1,11 @@
 const dialogConfig = {
-    dialogTitle: "[[" + Core.bundle.get("schematic") + "] schem.name()",
-    closeButtonEnabled: true,
     schematicInfoText: Core.bundle.format("schematic.info", schem.width, schem.height, schem.tiles.size),
     schematicImageMaxSize: 800,
     imageRowItems: 4,
     padding: {
         left: 2,
         right: 4,
-        image: Vars.iconMed,
     },
-    textColor: Color.lightGray,
 };
 
 module.exports = dialogConfig;
