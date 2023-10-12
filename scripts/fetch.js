86a5cddd7c354de322ad8cad615c09551f638080
BaiFengLinShi <115349874+BaiFengLinShi@users.noreply.github.com>
2023-10-12 15:32:58 +0800

    Update fetch.js
    optimized code

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,20 +1,12 @@
+const { getRepos } = require("schem-browser/settings");
+
 let schems = {
     object: {},
     array: [],
-    sync: () => {
-        let target = schems["object"]
-        let targetArr = schems["array"]
-        Object.keys(target).forEach(repo => {
-            schems["array"] = schems["array"].concat(target[repo]);
-        });
-    },
+    sync: () => { schems.array = Object.values(schems.object).flat(); },
     loadedRepos: [],
     errorRepos: []
 }
-
-let {
-    getRepos
-} = require("schem-browser/settings");
 
 function epicFail(e) {
     Log.info("Repository could not be reached. " + e);
@@ -22,56 +14,39 @@ function epicFail(e) {
 
 function handleRepo(repo, result) {
     schems.loadedRepos.push(repo);
-    let file = Vars.tmpDirectory.child(repo.replace("/", "") + ".zip");
-    file.writeBytes(result.getResult());
-    
-    let zip = new ZipFi(file);
-    
+
+    const fileName = repo.replace("/", "") + ".zip";
+    const filePath = Vars.tmpDirectory.child(fileName);
+    filePath.writeBytes(result.getResult());
     schems.object[repo] = [];
-    zip.walk(cons(f => {
+    new ZipFi(filePath).walk(f => {
         if (f.extEquals("msch")) {
             try {
-                let schem = Schematics.read(f);
-                schems.object[repo].push(schem);
+                schems.object[repo].push(Schematics.read(f));
             } catch (e) {
                 if (!schems.errorRepos.includes(repo)) schems.errorRepos.push(repo);
             }
         }
-    }));
-    file.delete();
+    });
+    filePath.delete();
 }
 
 module.exports = {
-    schemsObj: () => {
-        return schems.object;
-    },
-    schems: () => {
-        schems.sync();
-        return schems.array;
-    },
-    loadedRepos: () => {
-        return schems.loadedRepos;
-    },
-    errorRepos: () => {
-        return schems.errorRepos;
-    },
+    schemsObj: () => schems.object,
+    schems: () => (schems.sync(), schems.array),
+    loadedRepos: () => schems.loadedRepos,
+    errorRepos: () => schems.errorRepos,
     fetch: () => {
         let repos = getRepos();
         repos.forEach(repo => {
-            Log.info("Trying repo: " + repo + " @ " + Vars.ghApi + "/repos/" + repo + "/zipball/main");
-            Http.get(Vars.ghApi + "/repos/" + repo + "/zipball/main", loc => {
-
-                if (loc.getHeader("Location") != null) {
-                    Http.get(loc.getHeader("Location"), result => {
-                        handleRepo(repo, result)
-                    }, epicFail);
-                } else {
-                    handleRepo(repo, loc);
-
-                }
-
-            },
-                epicFail);
+            let repoUrl = Vars.ghApi + "/repos/" + repo + "/zipball/main";
+            let handleLocation = (location) => {
+                let handleResult = (result) => handleRepo(repo, result);
+                location.getHeader("Location") != null
+                  ? Http.get(location.getHeader("Location"), handleResult, epicFail)
+                  : handleResult(location);
+            };
+            Http.get(repoUrl, handleLocation, epicFail);
         });
     }
 }
