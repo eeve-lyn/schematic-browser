8997727679308e4eea0b59997442a5ce63e00963
eve <143365819+eeve-lyn@users.noreply.github.com>
2023-10-12 06:52:46 -0300

    i need that extra space i SWEAR

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,10 +1,13 @@
-const filters = { text: "", desc: "" };
+const filters = { text: "", desc: "", planet: 0};
 
 const textFilter = (schem) => filters.text === "" || schem.name().toLowerCase().includes(filters.text.toLowerCase());
 const descFilter = (schem) => filters.desc === "" || schem.description().toLowerCase().includes(filters.desc.toLowerCase());
+const planetFilter = (schem) => {
+    if (filters.planet == 0) return false;
+}
 
 module.exports = {
-    filter: (schem) => textFilter(schem) && descFilter(schem),
+    filter: (schem) => textFilter(schem) && descFilter(schem) && planetFilter(schem),
     setFilter: (filter, value) => (filters[filter] = value),
     getFilter: (filter) => filters[filter]
 };
