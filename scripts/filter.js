f2ae7967f98dd822970a52db1fed462ff2ab03db
BaiFengLinShi <115349874+BaiFengLinShi@users.noreply.github.com>
2023-10-14 02:16:08 +0800

    Update filter.js

--- scripts/filter.js
+++ scripts/filter.js
@@ -3,10 +3,11 @@ const filters = { text: "", desc: "", planet: 0};
 const textFilter = (schem) => filters.text === "" || schem.name().toLowerCase().includes(filters.text.toLowerCase());
 const descFilter = (schem) => filters.desc === "" || schem.description().toLowerCase().includes(filters.desc.toLowerCase());
 const planetFilter = (schem) => {
-    const serpulo = schem.requirements().toSeq().anyMatch(i => Items.serpuloItems.contains(i.item) && !Items.erekirItems.contains(i.item));
-    const erekir = schem.requirements().toSeq().anyMatch(i => Items.erekirItems.contains(i.item) && !Items.serpuloItems.contains(i.item));
-    return [true, serpulo && !erekir, !serpulo && erekir, serpulo && erekir][filters.planet];
+    const serpulo = schem.requirements().toSeq().anyMatch(i => Items.serpuloItems.contains(i.item));
+    const erekir = schem.requirements().toSeq().anyMatch(i => Items.erekirItems.contains(i.item));
+    return filters.planet === 0 || (filters.planet === 1 ? serpulo && !erekir : filters.planet === 2 ? !serpulo && erekir : filters.planet === 3 ? serpulo && erekir : false);
 }
+
 
 
 module.exports = {
