eff244c1692ade04337508070dfef0a104723e84
BaiFengLinShi <115349874+BaiFengLinShi@users.noreply.github.com>
2023-10-14 02:19:14 +0800

    Update filter.js

--- scripts/filter.js
+++ scripts/filter.js
@@ -3,11 +3,10 @@ const filters = { text: "", desc: "", planet: 0};
 const textFilter = (schem) => filters.text === "" || schem.name().toLowerCase().includes(filters.text.toLowerCase());
 const descFilter = (schem) => filters.desc === "" || schem.description().toLowerCase().includes(filters.desc.toLowerCase());
 const planetFilter = (schem) => {
-    const serpulo = schem.requirements().toSeq().anyMatch(i => Items.serpuloItems.contains(i.item));
-    const erekir = schem.requirements().toSeq().anyMatch(i => Items.erekirItems.contains(i.item));
+    let serpulo = schem.requirements().toSeq().anyMatch(i => Items.serpuloItems.contains(i.item) && !Items.erekirItems.contains(i.item));
+    let erekir = schem.requirements().toSeq().anyMatch(i => Items.erekirItems.contains(i.item) && !Items.serpuloItems.contains(i.item));
     return filters.planet === 0 || (filters.planet === 1 ? serpulo && !erekir : filters.planet === 2 ? !serpulo && erekir : filters.planet === 3 ? serpulo && erekir : false);
 }
-
 
 
 module.exports = {
