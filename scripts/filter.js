28861c83cb1c153e3905865577445b8406ec0c9f
BaiFengLinShi <115349874+BaiFengLinShi@users.noreply.github.com>
2023-10-14 02:21:49 +0800

    Update filter.js

--- scripts/filter.js
+++ scripts/filter.js
@@ -3,9 +3,8 @@ const filters = { text: "", desc: "", planet: 0};
 const textFilter = (schem) => filters.text === "" || schem.name().toLowerCase().includes(filters.text.toLowerCase());
 const descFilter = (schem) => filters.desc === "" || schem.description().toLowerCase().includes(filters.desc.toLowerCase());
 const planetFilter = (schem) => {
-    let serpulo = schem.requirements().toSeq().anyMatch(i => Items.serpuloItems.contains(i.item) && !Items.erekirItems.contains(i.item));
-    let erekir = schem.requirements().toSeq().anyMatch(i => Items.erekirItems.contains(i.item) && !Items.serpuloItems.contains(i.item));
-    return filters.planet === 0 || (filters.planet === 1 ? serpulo && !erekir : filters.planet === 2 ? !serpulo && erekir : filters.planet === 3 ? serpulo && erekir : false);
+    const items = schem.requirements().toSeq().toArray().map(i => [Items.serpuloItems.contains(i.item), Items.erekirItems.contains(i.item)]);
+    return [true, [1, 0], [0, 1], [1, 1]][filters.planet].every((val, i) => val ? items.every(arr => arr[i]) : true);
 }
 
 
