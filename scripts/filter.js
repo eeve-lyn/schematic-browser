4ba8059737e45784e824552aec9ddafddd428434
BaiFengLinShi <115349874+BaiFengLinShi@users.noreply.github.com>
2023-10-14 02:36:26 +0800

    Update filter.js

--- scripts/filter.js
+++ scripts/filter.js
@@ -3,9 +3,9 @@ const filters = { text: "", desc: "", planet: 0};
 const textFilter = (schem) => filters.text === "" || schem.name().toLowerCase().includes(filters.text.toLowerCase());
 const descFilter = (schem) => filters.desc === "" || schem.description().toLowerCase().includes(filters.desc.toLowerCase());
 const planetFilter = (schem) => {
-    let serpulo = schem.requirements().toSeq().some(i => Items.serpuloItems.contains(i.item) && !Items.erekirItems.contains(i.item));
-    let eerekir = schem.requirements().toSeq().some(i => Items.erekirItems.contains(i.item) && !Items.serpuloItems.contains(i.item));
-    return [1, serpulo && !eerekir, !serpulo && eerekir, serpulo && eerekir][filters.planet] || false;
+    let serpulo = schem.requirements().toSeq().contains(boolf(i => Items.serpuloItems.contains(i.item) && !Items.erekirItems.contains(i.item)));
+    let erekir = schem.requirements().toSeq().contains(boolf(i => Items.erekirItems.contains(i.item) && !Items.serpuloItems.contains(i.item)));
+    return [1, serpulo && !erekir, !serpulo && erekir, serpulo && erekir][filters.planet] || false;
 };
 
 
