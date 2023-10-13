4965d24e37946258c506763939c0258d6ae40675
BaiFengLinShi <115349874+BaiFengLinShi@users.noreply.github.com>
2023-10-14 02:28:16 +0800

    Update filter.js

--- scripts/filter.js
+++ scripts/filter.js
@@ -3,7 +3,7 @@ const filters = { text: "", desc: "", planet: 0};
 const textFilter = (schem) => filters.text === "" || schem.name().toLowerCase().includes(filters.text.toLowerCase());
 const descFilter = (schem) => filters.desc === "" || schem.description().toLowerCase().includes(filters.desc.toLowerCase());
 const planetFilter = (schem) => {
-    const req = s.requirements().toSeq().toArray();
+    const req = schem.requirements().toSeq().toArray();
     const serpulo = req.some(i => Items.serpuloItems.contains(i.item));
     const eerekir = req.some(i => Items.erekirItems.contains(i.item));
     return [1, serpulo && !eerekir, !serpulo && eerekir, serpulo && eerekir][filters.planet] || false;
