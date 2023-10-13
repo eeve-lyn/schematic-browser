3e79a7315140d10c46e0fa5888d4209652845b2a
BaiFengLinShi <115349874+BaiFengLinShi@users.noreply.github.com>
2023-10-14 02:27:14 +0800

    Update filter.js

--- scripts/filter.js
+++ scripts/filter.js
@@ -2,9 +2,11 @@ const filters = { text: "", desc: "", planet: 0};
 
 const textFilter = (schem) => filters.text === "" || schem.name().toLowerCase().includes(filters.text.toLowerCase());
 const descFilter = (schem) => filters.desc === "" || schem.description().toLowerCase().includes(filters.desc.toLowerCase());
-const planetFilter = schem => {
-    const [serpulo, erekir] = ["serpuloItems", "erekirItems"].map(itemList => schem.requirements().toSeq().anyMatch(i => Items[itemList].contains(i.item)));
-    return filters.planet === 0 || (filters.planet === 1 ? serpulo && !erekir : filters.planet === 2 ? !serpulo && erekir : filters.planet === 3 ? serpulo && erekir : false);
+const planetFilter = (schem) => {
+    const req = s.requirements().toSeq().toArray();
+    const serpulo = r.some(i => Items.serpuloItems.contains(i.item));
+    const eerekir = r.some(i => Items.erekirItems.contains(i.item));
+    return [1, serpulo && !eerekir, !serpulo && eerekir, serpulo && eerekir][filters.planet] || false;
 };
 
 
